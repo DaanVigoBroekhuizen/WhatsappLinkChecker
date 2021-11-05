6d2383e2ebae419632ede64f6a68e680bd7d7d3a
Justinnnp <2099449@talnet.nl>
2021-11-05 15:16:13 +0100

    pdo connection fixed and added a index.php file.

---
+++ site/db/database.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "127.0.0.1";
+$username = "root";
+$password = "";
+$db = "whatsapp";
+
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$db", "$username", "$password");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connected xd";
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
